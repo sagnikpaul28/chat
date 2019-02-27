f38354ea320084cc0a2815abd8e471820b915f9c
Sagnik Paul <sagnikpaul2882@gmail.com>
2019-02-27 13:06:11 +0530

    fixed recipents chatlist update

--- client/src/app/components/ChatDashboardChatContainer.js
+++ client/src/app/components/ChatDashboardChatContainer.js
@@ -4,11 +4,92 @@ import ChatDashboardChatMessageInput from "./ChatDashboardChatMessageInput";
 import ChatDashboardChatBody from "./ChatDashboardChatBody";
 import ChatDashboardChatHeader from "./ChatDashboardChatHeader";
 import ChatDashboardChatNotSelected from "./ChatDashboardChatNotSelected";
+import io from "socket.io-client";
+import {saveChatMessages} from "../actions/chatActions";
+import {saveChatList, saveExceptChatList} from "../actions/dashboardActions";
 
 class ChatDashboardChatContainer extends React.Component {
     constructor(props) {
         super(props);
+        let self = this;
+        self.chatListHTML = '';
+        self.socket = io('http://localhost:4000');
+        self.socket.on('message', function(data) {
+
+            let addToChatListUsername = "";
+            let chatList = self.props.userData.userData.chatList;
+            let chatListUsernames = [];
+
+            if ((data.sentBy === self.props.userData.userData.username && data.sentTo === self.props.chat.selectedUsername) || (data.sentTo === self.props.userData.userData.username && data.sentBy === self.props.chat.selectedUsername)) {
+                let newSelectedChatList = JSON.parse(self.props.chat.selectedChatList);
+                newSelectedChatList.push({
+                    message: data.message,
+                    sentBy: data.sentBy,
+                    time: data.time
+                });
+                newSelectedChatList = JSON.stringify(newSelectedChatList);
+                self.props.saveChatMessage(newSelectedChatList);
+            }
+
+            if (data.sentBy === self.props.userData.userData.username) {
+                addToChatListUsername = data.sentTo
+            }else if (data.sentTo === self.props.userData.userData.username) {
+                addToChatListUsername = data.sentBy;
+            }
+
+            if (addToChatListUsername) {
+                console.log(chatList);
+                for (let i = 0; i< chatList.length; i++) {
+                    if (chatList[i].username !== addToChatListUsername) {
+                        chatListUsernames.push(chatList[i].username);
+                    }
+                }
+                chatListUsernames.unshift(addToChatListUsername);
+                chatListUsernames = chatListUsernames.toString();
+                console.log(chatListUsernames);
+
+                fetch('http://localhost:4000/api/getChatListUserDetails', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        chatList: chatListUsernames
+                    })
+                })
+                    .then(res => res.json())
+                    .then(res => {
+                        console.log(res);
+                        self.props.saveChatList(
+                            res.map(item => {
+                                item.status = 'offline';
+                                return item;
+                            })
+                        );
+                    });
+
+                fetch('http://localhost:4000/api/getContactsExceptChatListDetails', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({
+                        chatList: chatListUsernames + "," + self.props.userData.userData.username
+                    })
+                })
+                    .then(res => res.json())
+                    .then(res => {
+                        self.props.saveExceptChatList(
+                            res.map(item => {
+                                item.status = 'offline';
+                                return item;
+                            })
+                        );
+                    });
+            }
+        });
     }
+
     render() {
         if (this.props.chat.isChatSelected) {
             return (
@@ -30,13 +111,29 @@ class ChatDashboardChatContainer extends React.Component {
 
 const mapStateToProps = (state) => {
     return {
+        userData: state.userDataReducer,
+        dashboardData: state.dashboardReducers,
         chat: state.chatReducers
     }
 };
 
 const mapDispatchToProps = (dispatch) => {
     return {
-
+        saveChatMessage: (chat) => {
+            dispatch(
+                saveChatMessages(chat)
+            )
+        },
+        saveExceptChatList: (data) => {
+            dispatch(
+                saveExceptChatList(data)
+            )
+        },
+        saveChatList: (data) => {
+            dispatch(
+                saveChatList(data)
+            )
+        },
     }
 };
 
